87bee83eb5523755aa2a69a73be108fc484d8715
dbustanUCSC <dbustan@ucsc.edu>
2023-10-18 11:47:23 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -22,14 +22,24 @@ canvas.style.boxShadow = "1rem 1rem 20px grey";
 canvas.style.border = "10px solid black";
 canvas.style.cursor = "none";
 const ctx = canvas.getContext("2d");
+
 ctx!.fillStyle = "white";
 ctx!.fillRect(rectXorigin, rectYorigin, canvasWidth, canvasHeight);
 
 const clearButton = document.createElement("button");
+const undoButton = document.createElement("button");
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "Redo.";
+undoButton.innerHTML = "Undo.";
 clearButton.innerHTML = "clear!";
 
+const num1 = 1;
+const num0 = 0;
+const event = new Event("drawing-changed");
 const cursor = { active: false, x: 0, y: 0 };
 let arraysOfCurrentLines: { x: number; y: number }[][] = [];
+const redoLines: { x: number; y: number }[][] = [];
+
 let line: { x: number; y: number }[] = [];
 
 canvas.addEventListener("mousedown", (mouseInfo) => {
@@ -46,15 +56,13 @@ canvas.addEventListener("mousemove", (mouseInfo) => {
     cursor.x = mouseInfo.offsetX;
     cursor.y = mouseInfo.offsetY;
     line.push({ x: cursor.x, y: cursor.y });
-    const event = new Event("drawing-changed");
+
     dispatchEvent(event);
   }
 });
 
 addEventListener("drawing-changed", handleDrawing);
 function handleDrawing() {
-  const num1 = 1;
-  const num0 = 0;
   ctx!.clearRect(rectXorigin, rectYorigin, canvasWidth, canvasHeight);
   for (const line of arraysOfCurrentLines) {
     if (line.length > num1) {
@@ -81,6 +89,22 @@ clearButton.addEventListener("click", () => {
   ctx!.clearRect(rectXorigin, rectYorigin, canvasWidth, canvasHeight);
   arraysOfCurrentLines = [];
 });
+
+undoButton.addEventListener("click", () => {
+  if (arraysOfCurrentLines.length > num0) {
+    redoLines.push(arraysOfCurrentLines.pop()!);
+    dispatchEvent(event);
+  }
+});
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > num0) {
+    arraysOfCurrentLines.push(redoLines.pop()!);
+    dispatchEvent(event);
+  }
+});
 app.append(canvas);
 app.append(header);
 app.append(clearButton);
+app.append(undoButton);
+app.append(redoButton);
